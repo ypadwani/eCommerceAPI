26cedabd71a5bc4d465f3f309a39a65c4a24953d
Yash Padwani <ypadwani@myseneca.ca>
2025-01-21 17:29:07 -0500

    mongoDB connection

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+    )
+    
+    .then(() => console.log("Db connection successful"))
+    .catch((err) => {
+        console.log(err)
+    });
+
+
+app.listen(process.env.PORT || 5001, () => {
+    console.log("Backend server is running!");
+});
